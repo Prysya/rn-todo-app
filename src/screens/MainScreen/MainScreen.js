a8bee25bb0150c7f7d610f5e798e1276e1458b01
Stas <bloodofabyss@yandex.ru>
2022-02-14 17:29:01 +0300

    feat: add Loader, error component, effect for fetching todos list

--- src/screens/MainScreen/MainScreen.js
+++ src/screens/MainScreen/MainScreen.js
@@ -1,19 +1,30 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { Dimensions, FlatList, Image, StyleSheet, View } from 'react-native';
 import { AddTodo } from '../../components/AddTodo/AddTodo';
+import { AppLoader } from '../../components/AppLoader/AppLoader';
 import { Todo } from '../../components/Todo/Todo';
 import { THEME } from '../../constants/Theme/theme';
 import { ScreenContext } from '../../context/screen/screenContext';
 import { TodoContext } from '../../context/todo/todoContext';
+import { AppButton } from '../../uikit/AppButton/AppButton';
+import { AppText } from '../../uikit/AppText/AppText';
 import { AppTextBold } from '../../uikit/AppTextBold/AppText';
 
 export const MainScreen = () => {
-  const { addTodo, todos, removeTodo } = useContext(TodoContext);
+  const { addTodo, todos, removeTodo, fetchTodos, loading, error } = useContext(TodoContext);
   const { changeScreen } = useContext(ScreenContext);
   
   const [deviceWidth, setDeviceWidth] = useState(
     Dimensions.get('window').width - THEME.PADDING_HORIZONTAL * 2,
   );
+  
+  const loadTodos = useCallback(async () => {
+    await fetchTodos();
+  }, [fetchTodos]);
+  
+  useEffect(() => {
+    loadTodos();
+  }, []);
   
   useEffect(() => {
     const update = () => {
@@ -25,6 +36,19 @@ export const MainScreen = () => {
     
     return () => Dimensions.removeEventListener('change', update);
   }, []);
+  
+  if (loading) {
+    return <AppLoader />;
+  }
+  
+  if (error) {
+    return (
+      <View style={styles.center}>
+        <AppText style={styles.error}>{error}</AppText>
+        <AppButton onPress={loadTodos}>Повторить</AppButton>
+      </View>
+    );
+  }
   
   let content = (
     <View style={{ width: deviceWidth }}>
@@ -41,7 +65,7 @@ export const MainScreen = () => {
       />
     </View>
   );
-  
+
   if (todos.length === 0) {
     content = (
       <View style={styles.imgWrap}>
@@ -75,5 +99,15 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: THEME.MAIN_COLOR,
     opacity: 0.7,
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  error: {
+    fontSize: 20,
+    color: THEME.DANGER_COLOR,
+    marginBottom: 10,
   },
 });
