cede9f0437dd6d18f72950c44828ce3d42eb9a2c
Stas <bloodofabyss@yandex.ru>
2022-02-14 17:28:09 +0300

    feat: add new styles for children centering

--- src/components/MainLayout/MainLayout.js
+++ src/components/MainLayout/MainLayout.js
@@ -10,9 +10,9 @@ export const MainLayout = () => {
   const { todoId } = useContext(ScreenContext);
   
   return (
-    <View>
+    <View style={styles.wrapper}>
       <Navbar title="Todo App!" />
-      
+  
       <View style={styles.container}>{todoId ? <TodoScreen /> : <MainScreen />}</View>
     </View>
   );
@@ -22,5 +22,9 @@ const styles = StyleSheet.create({
   container: {
     paddingHorizontal: THEME.PADDING_HORIZONTAL,
     paddingVertical: 20,
+    flex: 1,
+  },
+  wrapper: {
+    flex: 1,
   },
 });
